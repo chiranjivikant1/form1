12003488868aad35977dca8a83e6239f6abe693b
chianjivikant1 <1305271@kiit.ac.in>
2021-08-26 01:04:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RegistrationForm from "./components/RegistrationForm";
+
+const App = () => {
+	return (
+		<div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
+			<RegistrationForm />
+		</div>
+	);
+};
+
+export default App;
